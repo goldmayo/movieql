bdd899a5aaeb917915f05ef36b9dedb141dc1d49
HyunSJ <dudtod1596@gmail.com>
2021-07-02 17:37:31 +0900

    chore : update functions
    deleted:    index.js
    modified:   package.json
    renamed:    graphql/db.js -> src/graphql/db.js
    renamed:    graphql/resolvers.js -> src/graphql/resolvers.js
    renamed:    graphql/schema.graphql -> src/graphql/schema.graphql
    renamed:    graphql/test_db.js -> src/graphql/test_db.js
    new file:   src/index.js
    
    end

--- src/graphql/db.js
+++ src/graphql/db.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+// const BASE_URL = "http://yts.mx/api/v2/";
 const BASE_URL = "https://yts-proxy.now.sh/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIES_DETAILS_URL = `${BASE_URL}movie_details.json`;
@@ -9,10 +10,16 @@ export const getMovies = async (limit, rating) => {
     data: {
       data: { movies },
     },
-  } = await axios(LIST_MOVIES_URL, {
+  } = await axios.get(LIST_MOVIES_URL, {
     params: {
       limit,
       minimum_rating: rating,
+    },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-Forwarded-Proto": "https",
+      Origin: "*",
     },
   });
   return movies;
@@ -23,7 +30,7 @@ export const getMovie = async (id) => {
     data: {
       data: { movie },
     },
-  } = await axios(MOVIES_DETAILS_URL, {
+  } = await axios.get(MOVIES_DETAILS_URL, {
     params: {
       movie_id: id,
     },
@@ -36,7 +43,7 @@ export const getSuggestions = async (id) => {
     data: {
       data: { movies },
     },
-  } = await axios(MOVIES_SUGGESTIONS_URL, {
+  } = await axios.get(MOVIES_SUGGESTIONS_URL, {
     params: {
       movie_id: id,
     },
